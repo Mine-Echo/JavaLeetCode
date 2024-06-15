6e2e2e4aa0d2bda1d8b93ca025141daad5f39e0d
Echo <2637578244@qq.com>
2024-06-15 17:52:20 +0800

    Add LeetCode80

---
+++ Classic-Interview-150/LeetCode80.java
@@ -0,0 +1,17 @@
+//删除有序数组中的重复项2
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //还是用双指针法
+        if(nums.length<=2)
+            return nums.length;
+        int left=2,right=2;
+        for(;right<nums.length;right++){
+            // if(nums[right]!=nums[left-1]||nums[left-1]!=nums[left-2]){
+            if(nums[left-2]!=nums[right]){//判断条件可以简化成这样，我写的判断条件还是复杂了一点
+                nums[left++]=nums[right];
+            }
+        }
+        return left;
+    }
+}
