437b2c377906e5bedca7f60c802cd604c72b5b4b
Echo <2637578244@qq.com>
2024-06-16 16:00:43 +0800

    Add LeetCode55

---
+++ Classic-Interview-150/LeetCode55.java
@@ -0,0 +1,26 @@
+//跳跃游戏
+class Solution {
+    public boolean canJump(int[] nums) {
+        // 从后往前判断很容易
+        if (nums.length < 2)
+            return true;
+        int position = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] >= position - i)
+                position = i;
+        }
+        if (position == 0)
+            return true;
+        else
+            return false;
+
+        // 看了题解发现正着来也很简单，自己实现了一下
+        // int rightest = 0;
+        // for (int i = 0; i <= rightest; i++) {
+        //     rightest = Math.max(rightest, i + nums[i]);
+        //     if (rightest >= nums.length - 1)
+        //         return true;
+        // }
+        // return false;
+    }
+}
