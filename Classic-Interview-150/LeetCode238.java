ca9ad7837e9ee6a9532ce6b305d4f57750fcfb26
Echo <2637578244@qq.com>
2024-06-28 18:09:18 +0800

    Add LeetCode238

---
+++ Classic-Interview-150/LeetCode238.java
@@ -0,0 +1,39 @@
+//除自身以外的数组的乘积
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // 方法1
+        // int[] pre = new int[nums.length];
+        // int[] post = new int[nums.length];
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i == 0)
+        //         pre[i] = 1;
+        //     else
+        //         pre[i] = pre[i - 1] * nums[i - 1];
+        // }
+        // for (int i = nums.length - 1; i >= 0; i--) {
+        //     if (i == nums.length - 1)
+        //         post[i] = 1;
+        //     else
+        //         post[i] = post[i + 1] * nums[i + 1];
+        // }
+        // int[] result = new int[nums.length];
+        // for (int i = 0; i < nums.length; i++) {
+        //     result[i] = pre[i] * post[i];
+        // }
+        // return result;
+
+        // 方法2，O1空间复杂度，用输出数组存pre，然后动态构造post
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+        return result;
+    }
+}
