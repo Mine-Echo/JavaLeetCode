5c885de9b569481efe10826d4b01b12982686d74
Echo <2637578244@qq.com>
2024-08-06 11:33:22 +0800

    Add LeetCode100

---
+++ Classic-Interview-150/LeetCode100.java
@@ -0,0 +1,36 @@
+//相同的树
+
+public class LeetCode100 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null){
+            return true;
+        }else if(p!=null&&q!=null){
+            if(p.val!=q.val)
+                return false;
+            else
+                return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        }else{
+            return false;
+        }
+    }
+}
