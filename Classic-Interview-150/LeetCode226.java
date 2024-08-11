e85f23edcf87a9d203dbc724b0df735c71a4a6a9
Echo <2637578244@qq.com>
2024-08-11 11:42:34 +0800

    Add LeetCode226

---
+++ Classic-Interview-150/LeetCode226.java
@@ -0,0 +1,33 @@
+//翻转二叉树
+
+public class LeetCode226 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+}
